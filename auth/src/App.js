b82a8e6d55f9b8533ced11add9e03aca4aed5ec8
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-11-18 17:40:34 +0700

    yoloface

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA5SRye6xr3DJuTw0abDzlYV9Cahs6xkDs',
+      authDomain: 'authentication-e2676.firebaseapp.com',
+      databaseURL: 'https://authentication-e2676.firebaseio.com',
+      storageBucket: '',
+      messagingSenderId: '626538201406'
+    });
+  }
+
   render() {
     return (
       <View>
           <Header headerText="Authentication" />
-          <Text>Yes</Text>
+          <LoginForm />
       </View>
     );
   }
