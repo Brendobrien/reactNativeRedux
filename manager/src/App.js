05f251dadc7d4859884210dc73a368b2ab6bf363
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-11-24 16:33:44 +0700

    'yeaha;sldfioj'

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Text } from 'react-native';
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const App = () => {
-  return (
-    <Text>Hello</Text>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(()=>[])}>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
 
 export default App;
