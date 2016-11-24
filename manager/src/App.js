61de33641fc35ee05a2988dd732cfaa8db070b72
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-11-24 16:16:07 +0700

    she says she loves me. i dont know about love

---
+++ manager/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+
+const App = () => {
+  return (
+    <Text>Hello</Text>
+  );
+};
+
+export default App;
