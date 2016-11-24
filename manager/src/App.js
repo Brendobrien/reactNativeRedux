c4d661930cf8d3466d52c292fa20cb7679172c7e
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-11-24 16:44:45 +0700

    sweet home alabama

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCbPiNo9uocupoqPlk-eRYmt6FJabeA3Ec',
+      authDomain: 'manager-18993.firebaseapp.com',
+      databaseURL: 'https://manager-18993.firebaseio.com',
+      storageBucket: 'manager-18993.appspot.com',
+      messagingSenderId: '386102119466'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore(()=>[])}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello</Text>
         </View>
